396fa61e2bb81a7331606cf1337d4be3cb21fdef
loreV <lore.verri@gmail.com>
2019-04-24 01:02:30 +0200

    BK-0007 Added implementation to listener/subject

---
+++ onboard/src/main/java/org/kibe/onboard/communication/board/DataCommReceiver.java
@@ -0,0 +1,10 @@
+package org.kibe.onboard.communication.board;
+
+import org.kibe.common.data.DataPoint;
+
+public class DataCommReceiver implements DataListener {
+    @Override
+    public void notifyForData(DataPoint dataPoint) {
+
+    }
+}
