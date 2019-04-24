6a23c51f86538fb1109173ad1ae643c65380569b
loreV <lore.verri@gmail.com>
2019-04-24 19:44:40 +0200

    BK-0007 Added controller-module to separate controller logic from generic module settings

--- onboard/src/main/java/org/kibe/onboard/configuration/ConfigurationManager.java
+++ onboard/src/main/java/org/kibe/onboard/configuration/ConfigurationManager.java
@@ -1,30 +1,29 @@
 package org.kibe.onboard.configuration;
 
 import com.google.inject.Inject;
-import com.google.inject.name.Named;
 import org.apache.logging.log4j.Logger;
 import org.kibe.common.exception.ConfigurationException;
 import org.kibe.onboard.configuration.module.BoardToIntelCommModule;
+import org.kibe.onboard.configuration.module.ControllerModule;
 import org.kibe.onboard.configuration.module.DataListenerModule;
 import org.kibe.onboard.configuration.module.I2CommunicationModule;
-import org.kibe.onboard.controller.CommunicationController;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 import static java.lang.String.format;
 import static org.apache.logging.log4j.LogManager.getLogger;
-import static spark.Spark.port;
 
 public class ConfigurationManager {
 
     private final Logger LOG = getLogger(ConfigurationManager.class.getName());
 
-    private static final String PORT_PROPERTY = "web-port";
-
+    /**
+     * Communication modules
+     */
     private final BoardToIntelCommModule boardToIntelCommModule;
-    private final CommunicationController restController;
     private final I2CommunicationModule i2commModule;
+    private final ControllerModule controllerModule;
 
     /**
      * Encapsulates data listeners
@@ -34,16 +33,13 @@ public class ConfigurationManager {
     @Inject
     public ConfigurationManager(
             final BoardToIntelCommModule boardToIntelCommModule,
-            final CommunicationController restController,
             final I2CommunicationModule i2CommunicationModule,
-            final DataListenerModule dataListenerModule,
-            // TODO: move this away from this generic config class
-            final @Named(PORT_PROPERTY) String port) {
+            final ControllerModule controllerModule,
+            final DataListenerModule dataListenerModule) {
         this.boardToIntelCommModule = boardToIntelCommModule;
-        this.restController = restController;
         this.i2commModule = i2CommunicationModule;
+        this.controllerModule = controllerModule;
         this.dataListenerModule = dataListenerModule;
-        port(Integer.parseInt(port));
     }
 
     public void init() throws ConfigurationException {
@@ -53,7 +49,8 @@ public class ConfigurationManager {
             throw new ConfigurationException(format("Error while initializing queue connection. Error: %s", e.getMessage()));
         }
         i2commModule.init(dataListenerModule.getDataListener());
-        restController.init();
+        controllerModule.init();
+        LOG.info("Initialization completed");
     }
 
 }
